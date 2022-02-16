4583afec7d71099e7bff7b55cc5b6a2b83cc656a
vitortcmiranda <vitortcmout@gmail.com>
2022-02-16 16:21:39 -0300

    Creating the database connection

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,9 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	runtimeOnly("mysql:mysql-connector-java")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/com/tcgstore/model/CustomerModel.kt
+++ src/main/kotlin/com/tcgstore/model/CustomerModel.kt
@@ -1,7 +1,7 @@
 package com.tcgstore.model
 
 data class CustomerModel(
-    var id: String? = null,
+    var id: Int? = null,
     var name: String,
     var email: String
 )
