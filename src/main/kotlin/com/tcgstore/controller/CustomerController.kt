c03aa9f36cc534bfc5faaaa7887075538ca2f07b
vitortcmiranda <vitortcmout@gmail.com>
2022-02-16 08:39:58 -0300

    Adding the list customers

--- src/main/kotlin/com/tcgstore/controller/CustomerController.kt
+++ src/main/kotlin/com/tcgstore/controller/CustomerController.kt
@@ -8,18 +8,22 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("customer")
 class CustomerController {
+
+    val customers = mutableListOf<CustomerModel>()
+
     @GetMapping
-    fun getCustomer(): CustomerModel {
-        return CustomerModel(
-            "1",
-            "Vitor",
-            "email@email.com"
-        )
+    fun getCustomer(): List<CustomerModel> {
+        return customers;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun createCustomer(@RequestBody customer: PostCustomerRequest) {
-        println(customer)
+        val id = if(customers.isEmpty()){
+            1
+        }else{
+            customers.last().id.toInt() + 1
+        }
+        customers.add(CustomerModel(id.toString(),customer.name,customer.email))
     }
 }
