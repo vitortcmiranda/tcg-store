1157d3a2ad69309df3a2279b8f1d41ca54f93f45
vitortcmiranda <vitortcmout@gmail.com>
2022-02-15 21:12:27 -0300

    Creating tcg store backend using kotlin and spring

---
+++ src/main/kotlin/com/tcgstore/controller/CustomerController.kt
@@ -0,0 +1,12 @@
+package com.tcgstore.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class CustomerController {
+    @GetMapping
+    fun helloWorld(): String{
+        return "Hello World"
+    }
+}
---
+++ src/test/kotlin/com/tcgstore/TcgStoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tcgstore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TcgStoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
