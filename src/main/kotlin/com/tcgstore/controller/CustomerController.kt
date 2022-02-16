8f4c3590f08e882bb5a5c1fc565dc5669633f029
vitortcmiranda <vitortcmout@gmail.com>
2022-02-15 22:00:21 -0300

    Post route to create clients

--- src/main/kotlin/com/tcgstore/controller/CustomerController.kt
+++ src/main/kotlin/com/tcgstore/controller/CustomerController.kt
@@ -2,6 +2,8 @@ package com.tcgstore.controller
 
 import com.tcgstore.model.CustomerModel
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,5 +17,9 @@ class CustomerController {
             "Vitor",
             "email@email.com"
         )
+    }
+    @PostMapping
+    fun createCustomer(@RequestBody customer: CustomerModel): CustomerModel {
+        return customer
     }
 }
