70cd7c01245cd3e95e145340e7210b5891f1942f
vitortcmiranda <vitortcmout@gmail.com>
2022-02-15 21:19:01 -0300

    Creating the first route

--- src/main/kotlin/com/tcgstore/controller/CustomerController.kt
+++ src/main/kotlin/com/tcgstore/controller/CustomerController.kt
@@ -1,12 +1,18 @@
 package com.tcgstore.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("api")
 class CustomerController {
-    @GetMapping
+    @GetMapping("/id")
     fun helloWorld(): String{
-        return "Hello World"
+        return "Customer 1"
+    }
+    @GetMapping("/all")
+    fun getAll(): String {
+        return "Client 1 , Client 2"
     }
 }
