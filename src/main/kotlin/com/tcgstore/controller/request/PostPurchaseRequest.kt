4e943ea1fc7e308b68f8801df954215c96b0b31f
vitortcmiranda <vitortcmout@gmail.com>
2022-03-05 09:26:46 -0300

    fixing the router and adding JsonAlias

--- src/main/kotlin/com/tcgstore/controller/request/PostPurchaseRequest.kt
+++ src/main/kotlin/com/tcgstore/controller/request/PostPurchaseRequest.kt
@@ -1,13 +1,16 @@
 package com.tcgstore.controller.request
 
+import com.fasterxml.jackson.annotation.JsonAlias
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Positive
 
 data class PostPurchaseRequest(
     @field:NotNull
     @field:Positive
+    @JsonAlias("customer_id") //camelCase -> snake_case
     val customerId: Int,
 
     @field:NotNull
+    @JsonAlias("card_ids")
     val cardIds: Set<Int>
 )
